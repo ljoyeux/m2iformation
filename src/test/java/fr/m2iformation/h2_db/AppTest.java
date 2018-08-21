63b7f4c9527b62ea79bca68390b95d69e13544a5
Administrateur <Administrateur@FORMATI-MDDIFHT>
2018-08-21 15:42:31 +0200

    Nettoyage de la classe de test AppTest

--- src/test/java/fr/m2iformation/h2_db/AppTest.java
+++ src/test/java/fr/m2iformation/h2_db/AppTest.java
@@ -1,25 +1,8 @@
 package fr.m2iformation.h2_db;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 import org.junit.Test;
 
 public class AppTest {
-
-	@Test
-	public void dummy() {
-		System.out.println("coucou");
-	}
-
-
-//	@Test
-	public void connectToDB() throws SQLException {
-		try (final Connection connection = DriverManager.getConnection(Constants.URL_JDBC, "sa", "")) {
-			assert connection != null;
-		}
-	}
 
 	@Test
 	public void connectToDBV2() {
